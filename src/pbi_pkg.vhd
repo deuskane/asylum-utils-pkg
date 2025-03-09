7e09adfdd498cc123d0b4f83e4952f5a441667a9
deuskane <deuskane@gmail.com>
2025-03-09 07:30:49 +0100

    use unconstrained pbi

--- src/pbi_pkg.vhd
+++ src/pbi_pkg.vhd
@@ -6,7 +6,7 @@
 -- Author     : mrosiere
 -- Company    : 
 -- Created    : 2017-03-15
--- Last update: 2025-01-12
+-- Last update: 2025-03-08
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -27,17 +27,18 @@ package pbi_pkg is
   constant PBI_DATA_WIDTH : natural := 8;
   
   type pbi_ini_t is record
-    re           : std_logic;                                   -- Have a valid read (READ_STROBE)
+    cs           : std_logic;                                   -- Have a valid read  (READ_STROBE)
+    re           : std_logic;                                   -- Have a valid read  (READ_STROBE)
     we           : std_logic;                                   -- Have a valid write (WRITE_STROBE)
-    addr         : std_logic_vector(PBI_ADDR_WIDTH-1 downto 0); -- Access address (PORT_ID)
-    wdata        : std_logic_vector(PBI_DATA_WIDTH-1 downto 0); -- Write data (OUT_PORT)
+    addr         : std_logic_vector;                            -- Access address     (PORT_ID)
+    wdata        : std_logic_vector;                            -- Write data         (OUT_PORT)
   end record pbi_ini_t;
 
   type pbi_tgt_t is record
     busy         : std_logic;                                   -- Additionnal port
-    rdata        : std_logic_vector(PBI_DATA_WIDTH-1 downto 0); -- Read data (IN_PORT)
+    rdata        : std_logic_vector;                            -- Read data          (IN_PORT)
   end record pbi_tgt_t;
-
+  
   function "and" (i0, i1 : pbi_ini_t) return pbi_ini_t;
   function "or"  (i0, i1 : pbi_ini_t) return pbi_ini_t;
   function "xor" (i0, i1 : pbi_ini_t) return pbi_ini_t;
@@ -51,8 +52,10 @@ end pbi_pkg;
 package body pbi_pkg is
 
   function "and" (i0, i1 : pbi_ini_t) return pbi_ini_t is
-    variable z : pbi_ini_t;
+    variable z : pbi_ini_t(addr (i0.addr 'range),
+                            wdata(i0.wdata'range));
   begin
+    z.cs    := i0.cs    and i1.cs    ;
     z.re    := i0.re    and i1.re    ;
     z.we    := i0.we    and i1.we    ;
     z.addr  := i0.addr  and i1.addr  ;
@@ -62,8 +65,10 @@ package body pbi_pkg is
   end function "and";
 
   function "or" (i0, i1 : pbi_ini_t) return pbi_ini_t is
-    variable z : pbi_ini_t;
+    variable z : pbi_ini_t(addr (i0.addr 'range),
+                            wdata(i0.wdata'range));
   begin
+    z.cs    := i0.cs    or i1.cs    ;
     z.re    := i0.re    or i1.re    ;
     z.we    := i0.we    or i1.we    ;
     z.addr  := i0.addr  or i1.addr  ;
@@ -73,8 +78,10 @@ package body pbi_pkg is
   end function "or";
 
   function "xor" (i0, i1 : pbi_ini_t) return pbi_ini_t is
-    variable z : pbi_ini_t;
+    variable z : pbi_ini_t(addr (i0.addr 'range),
+                            wdata(i0.wdata'range));
   begin
+    z.cs    := i0.cs    xor i1.cs    ;
     z.re    := i0.re    xor i1.re    ;
     z.we    := i0.we    xor i1.we    ;
     z.addr  := i0.addr  xor i1.addr  ;
@@ -84,7 +91,7 @@ package body pbi_pkg is
   end function "xor";
 
   function "and" (i0, i1 : pbi_tgt_t) return pbi_tgt_t is
-    variable z : pbi_tgt_t;
+    variable z : pbi_tgt_t(rdata(i0.rdata'range));
   begin
     z.busy  := i0.busy  and i1.busy  ;
     z.rdata := i0.rdata and i1.rdata ;
@@ -93,7 +100,7 @@ package body pbi_pkg is
   end function "and";
 
   function "or" (i0, i1 : pbi_tgt_t) return pbi_tgt_t is
-    variable z : pbi_tgt_t;
+    variable z : pbi_tgt_t(rdata(i0.rdata'range));
   begin
     z.busy  := i0.busy  or i1.busy  ;
     z.rdata := i0.rdata or i1.rdata ;
@@ -102,7 +109,7 @@ package body pbi_pkg is
   end function "or";
 
   function "xor" (i0, i1 : pbi_tgt_t) return pbi_tgt_t is
-    variable z : pbi_tgt_t;
+    variable z : pbi_tgt_t(rdata(i0.rdata'range));
   begin
     z.busy  := i0.busy  xor i1.busy  ;
     z.rdata := i0.rdata xor i1.rdata ;
