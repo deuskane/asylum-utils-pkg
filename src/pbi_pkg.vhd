987cbaa0cdcd84c219e8259eb39353258ac4360c
deuskane <deuskane@gmail.com>
2025-03-30 15:04:47 +0200

    Add type for unconstrainted array

--- src/pbi_pkg.vhd
+++ src/pbi_pkg.vhd
@@ -6,7 +6,7 @@
 -- Author     : mrosiere
 -- Company    : 
 -- Created    : 2017-03-15
--- Last update: 2025-03-08
+-- Last update: 2025-03-27
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -25,6 +25,10 @@ use ieee.numeric_std.all;
 package pbi_pkg is
   constant PBI_ADDR_WIDTH : natural := 8;
   constant PBI_DATA_WIDTH : natural := 8;
+
+  type pbi_addrs_t is array (natural range <>) of std_logic_vector(PBI_ADDR_WIDTH-1 downto 0);
+  type pbi_datas_t is array (natural range <>) of std_logic_vector(PBI_DATA_WIDTH-1 downto 0);
+
   
   type pbi_ini_t is record
     cs           : std_logic;                                   -- Have a valid read  (READ_STROBE)
@@ -47,6 +51,11 @@ package pbi_pkg is
   function "or"  (i0, i1 : pbi_tgt_t) return pbi_tgt_t;
   function "xor" (i0, i1 : pbi_tgt_t) return pbi_tgt_t;
 
+  type pbi_inis_t is array (natural range <>) of pbi_ini_t;
+  type pbi_tgts_t is array (natural range <>) of pbi_tgt_t;
+
+  function "or"  (i0 : pbi_tgts_t)    return pbi_tgt_t;
+
 end pbi_pkg;
 
 package body pbi_pkg is
@@ -66,7 +75,7 @@ package body pbi_pkg is
 
   function "or" (i0, i1 : pbi_ini_t) return pbi_ini_t is
     variable z : pbi_ini_t(addr (i0.addr 'range),
-                            wdata(i0.wdata'range));
+                           wdata(i0.wdata'range));
   begin
     z.cs    := i0.cs    or i1.cs    ;
     z.re    := i0.re    or i1.re    ;
@@ -116,5 +125,19 @@ package body pbi_pkg is
 
     return z;
   end function "xor";
+
+  function "or" (i0 : pbi_tgts_t) return pbi_tgt_t is
+    variable z : pbi_tgt_t(rdata(i0(0).rdata'range));
+  begin
+    z.busy  := '0';
+    z.rdata := (others => '0');
+
+    for i in i0'range loop
+      z := z or i0(i);
+    end loop;  -- i
+    
+    return z;
+  end function "or";
+
   
 end package body pbi_pkg;
