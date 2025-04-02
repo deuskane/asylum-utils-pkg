e05bfe2108bc78658ca77174e6b245f1327845ac
deuskane <deuskane@gmail.com>
2025-04-02 17:51:09 +0200

    string_pkg : cosmetics

--- src/string_pkg.vhd
+++ src/string_pkg.vhd
@@ -16,20 +16,20 @@ use ieee.numeric_std.all;
 
 package string_pkg is
 
-  function string_eq(s1 : string; s2 : string) return boolean; 
-  function string_ne(s1 : string; s2 : string) return boolean; 
-    
+  function string_eq (str1 : string; str2 : string) return boolean; 
+  function string_ne (str1 : string; str2 : string) return boolean; 
+  
 end string_pkg;
 
 package body string_pkg is
 
-  function string_eq(s1 : string; s2 : string) return boolean is
+  function string_eq(str1 : string; str2 : string) return boolean is
   begin
-    if s1'length /= s2'length then
+    if str1'length /= str2'length then
       return false;
     else
-      for i in s1'range loop
-        if s1(i) /= s2(i) then
+      for i in str1'range loop
+        if str1(i) /= str2(i) then
           return false;
         end if;
       end loop;
@@ -37,18 +37,18 @@ package body string_pkg is
     end if;
   end function;
 
-  function string_ne(s1 : string; s2 : string) return boolean is
+  function string_ne(str1 : string; str2 : string) return boolean is
   begin
-    if s1'length /= s2'length then
+    if str1'length /= str2'length then
       return true;
     else
-      for i in s1'range loop
-        if s1(i) /= s2(i) then
+      for i in str1'range loop
+        if str1(i) /= str2(i) then
           return true;
         end if;
       end loop;
       return false;
     end if;
   end function;
-
+ 
 end string_pkg;
