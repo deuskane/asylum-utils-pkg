4d723a5ba4eb128277b76143bd02e7b671fd28a8
deuskane <deuskane@gmail.com>
2025-11-29 06:02:36 +0100

    logic_pkg  : Add mux2 and reverse_bits

--- hdl/logic_pkg.vhd
+++ hdl/logic_pkg.vhd
@@ -9,6 +9,7 @@
 -- Revisions  :
 -- Date        Version  Author  Description
 -- 2025-03-19  1.0      mrosiere Created
+-- 2025-11-29  1.1      mrosiere Add mux2 and reverse_bits
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -16,9 +17,14 @@ use ieee.numeric_std.all;
 
 package logic_pkg is
 
-  function reduce_xor(x : std_logic_vector) return std_logic;
-  function reduce_and(x : std_logic_vector) return std_logic;
-  function reduce_or (x : std_logic_vector) return std_logic;
+  function reduce_xor  (x       : std_logic_vector) return std_logic;
+  function reduce_and  (x       : std_logic_vector) return std_logic;
+  function reduce_or   (x       : std_logic_vector) return std_logic;
+  function mux2        (sel     : boolean;
+                        d_true  : std_logic_vector;
+                        d_false : std_logic_vector) return std_logic_vector;
+  
+  function reverse_bits(v       : std_logic_vector) return std_logic_vector ;
 
 end logic_pkg;
 
@@ -51,4 +57,26 @@ package body logic_pkg is
     return z;
   end reduce_or;
     
+  function mux2(sel     : boolean;
+                d_true  : std_logic_vector;
+                d_false : std_logic_vector) return std_logic_vector is
+  begin
+    if sel
+    then
+      return d_true;
+    else
+      return d_false;
+    end if;
+  end function;
+
+  -- Function to reverse bits in a vector
+  function reverse_bits(v : std_logic_vector) return std_logic_vector is
+    variable res : std_logic_vector(v'range);
+  begin
+    for i in v'range loop
+      res(i) := v(v'length-1 - i);
+    end loop;
+    return res;
+  end function;
+
 end logic_pkg;
