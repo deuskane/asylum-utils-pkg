b377f300605214c26f58cb506423eb160be7242a
deuskane <deuskane@gmail.com>
2026-06-05 20:42:13 +0200

    convert_pkg: Add to_slv function

--- hdl/convert_pkg.vhd
+++ hdl/convert_pkg.vhd
@@ -9,6 +9,7 @@
 -- Revisions  :
 -- Date        Version  Author  Description
 -- 2025-03-19  1.0      mrosiere Created
+-- 2026-06-05  1.1      mrosiere Add to_slv function
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -17,7 +18,11 @@ use ieee.numeric_std.all;
 package convert_pkg is
 
   function to_stdulogic     ( V: boolean )
-    return std_ulogic; 
+    return std_ulogic;
+
+    -- Function to convert integer to std_logic_vector
+  function to_slv (val : natural; size : natural) 
+    return std_logic_vector;
 
   function onehot_to_integer(one_hot : std_logic_vector)
     return integer;
@@ -35,6 +40,13 @@ package body convert_pkg is
       return '0';
     end if;     
   end to_stdulogic;
+
+  function to_slv (val : natural; size : natural) return std_logic_vector is
+    variable res : std_logic_vector(size-1 downto 0);
+  begin
+    res := std_logic_vector(to_unsigned(val, size));
+    return res;
+  end function to_slv;
 
   function onehot_to_integer(one_hot : std_logic_vector) return integer  is
     variable result : integer := -1; -- Defaut value for invalid case
